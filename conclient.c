86db99253e26c1c900e81db29c4b2b2610a09a80
Prabhakaran <lttesp@users.noreply.github.com>
2017-05-23 12:16:30 +0530

    Comments Added

--- conclient.c
+++ conclient.c
@@ -1,21 +1,20 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <arpa/inet.h>
+#include <stdlib.h>     // Standard Definitions Library
+#include <stdio.h>      // For Standard I/O Operations like puts, gets
+#include <sys/types.h>  // For Various Data types
+#include <sys/socket.h> // For Various Socket Functions soc
+#include <netinet/in.h> // For Various Sockaddr_in6 structures like struct in6_ad
+#include <string.h>     // For various string operations
+#include <arpa/inet.h>  // Definitions for Internet Operations 
 
+//Constant Definitions
+#define MAXLINE 4096    //Maximum text line length
+#define SERV_PORT 3000  //Port number to communicate
 
-#define MAXLINE 4096 /*max text line length*/
-#define SERV_PORT 3000 /*port*/
-
-int
-main(int argc, char **argv) 
+int main(int argc, char **argv) 
 {
- int sockfd;
- struct sockaddr_in servaddr;
- char sendline[MAXLINE], recvline[MAXLINE];
+ int sockfd;            // Socket Descriptor Function
+ struct sockaddr_in servaddr; //IP Socket Address with 16 Bit Port Number 
+ char sendline[MAXLINE], recvline[MAXLINE]; // Maximum String line for messages.
 	
  //basic check of the arguments
  //additional checks can be inserted
@@ -33,29 +32,31 @@ main(int argc, char **argv)
 	
  //Creation of the socket
  memset(&servaddr, 0, sizeof(servaddr));
- servaddr.sin_family = AF_INET;
- servaddr.sin_addr.s_addr= inet_addr(argv[1]);
+ servaddr.sin_family = AF_INET; //Ipv4 Address Protocol Address Family
+ servaddr.sin_addr.s_addr= inet_addr(argv[1]); // IPv4 Server Address 
  servaddr.sin_port =  htons(SERV_PORT); //convert to big-endian order
 	
  //Connection of the client to the socket 
- if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr))<0) {
+ if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr))<0) { 
   perror("Problem in connecting to the server");
   exit(3);
  }
 	
- while (fgets(sendline, MAXLINE, stdin) != NULL) {
+ while (fgets(sendline, MAXLINE, stdin) != NULL) { //Given Condition is Checked if not equal to Null
 	
-  send(sockfd, sendline, strlen(sendline), 0);
+  send(sockfd, sendline, strlen(sendline), 0); // Send the Entered Message by the user
 		
-  if (recv(sockfd, recvline, MAXLINE,0) == 0){
+  if (recv(sockfd, recvline, MAXLINE,0) == 0){ // If Value is equal to Zero Then there is no connection.
    //error: server terminated prematurely
    perror("The server terminated prematurely"); 
    exit(4);
   }
-  printf("%s", "String received from the server: ");
-  fputs(recvline, stdout);
+  printf("%s", "String received from the server: "); //Prints the Recieved Message from the server 
+  fputs(recvline, stdout); //Saves the Message entered by the user.
  }
 
  exit(0);
 }
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
--- conserver.c
+++ conserver.c
@@ -1,25 +1,25 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <unistd.h>
+#include <stdlib.h>     // Standard Definitions Library 
+#include <stdio.h>      // For Standard Input/Output Operations
+#include <sys/types.h>  // For Various Data types
+#include <sys/socket.h> // For Various Socket Functions socket()
+#include <netinet/in.h> // For Various Sockaddr_in6 structures like struct in6_addr
+#include <string.h>     // FOr various string operations
+#include <unistd.h>     // For fork() Function 
 
-
-#define MAXLINE 4096 /*max text line length*/
-#define SERV_PORT 3000 /*port*/
-#define LISTENQ 8 /*maximum number of client connections*/
+//Constant Definitions
+#define MAXLINE 4096   /* max text line length*/
+#define SERV_PORT 3000 /* port number to communicate*/
+#define LISTENQ 8      /* maximum number of client connections*/
 
 int main (int argc, char **argv)
 {
- int listenfd, connfd, n;
- pid_t childpid;
- socklen_t clilen;
- char buf[MAXLINE];
+ int listenfd, connfd, n; // Socket Descriptor Function
+ pid_t childpid;          // Child Process ID 
+ socklen_t clilen;        
+ char buf[MAXLINE];       // String Message Buffer 
  struct sockaddr_in cliaddr, servaddr;
 
- //Create a socket for the soclet
+ //Create a socket for the solet
  //If sockfd<0 there was an error in the creation of the socket
  if ((listenfd = socket (AF_INET, SOCK_STREAM, 0)) <0) {
   perror("Problem in creating the socket");
@@ -28,45 +28,63 @@ int main (int argc, char **argv)
 
 
  //preparation of the socket address
- servaddr.sin_family = AF_INET;
- servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
- servaddr.sin_port = htons(SERV_PORT);
-
- //bind the socket
- bind (listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+ servaddr.sin_family = AF_INET;// Returns IP Address Family
+ servaddr.sin_addr.s_addr = htonl(INADDR_ANY); // IP Address 
+ servaddr.sin_port = htons(SERV_PORT);// Port Number
+ 
+ //bind the socket 
+ bind (listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr)); 
 
  //listen to the socket by creating a connection queue, then wait for clients
  listen (listenfd, LISTENQ);
 
- printf("%s\n","Server running...waiting for connections.");
+ printf("%s\n","Server running...waiting for connections."); // Listen
 
  for ( ; ; ) {
 
   clilen = sizeof(cliaddr);
+
   //accept a connection
   connfd = accept (listenfd, (struct sockaddr *) &cliaddr, &clilen);
 
   printf("%s\n","Received request...");
 
-  if ( (childpid = fork ()) == 0 ) {//if it’s 0, it’s child process
+  // fork() Which takes care of concurrent Client Connection
 
+  if ( (childpid = fork ()) == 0 ) {//if it’s 0, it’s child process
+    //Each child process handles Client Connection Request
+
+    //If Client is connected 
     printf ("%s\n","Child created for dealing with client requests");
 
     //close listening socket
     close (listenfd);
 
-    while ( (n = recv(connfd, buf, MAXLINE,0)) > 0)  {
+    while ( (n = recv(connfd, buf, MAXLINE,0)) > 0)  { // until the condition satisfies 
      printf("%s","String received from and resent to the client:");
-     puts(buf);
-     send(connfd, buf, n, 0);
+     puts(buf);//Saves the user's message or text
+     send(connfd, buf, n, 0); // initiate transmission of a message from the specified socket to its peer.
     }
 
-    if (n < 0)
+    if (n < 0) // If there is no message throw error.
       printf("%s\n", "Read error");
     exit(0);
   }
+  
   //close socket of the server
   close(connfd);
  }
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* For my reference 
+
+Function arguments
+
+send (int socket, const void *buffer, size_t length, int flags);
+socket () - creates socket
+htonl() - converts the unsigned integer hostlong from host byte order to network byte order
+htnos - converts the unsigned short integer hostshort from host byte order to network byte order.
+
+  /*
